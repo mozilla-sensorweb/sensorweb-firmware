42e8e0e65a56853ed5e0a22b8b221ef583986f48
Dave Hylands <dhylands@gmail.com>
2016-11-02 08:59:37 -0700

    Add blinky main

---
+++ flash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OPT=debug
+for opt in $@; do
+    if [ x"$opt" == x"--release" ]; then
+        OPT=release
+    fi
+done
+
+TARGET=thumbv7em-none-eabi
+ELF_DIR=target/${TARGET}/${OPT}
+FIRMWARE_ELF=${ELF_DIR}/sensorweb-firmware
+FIRMWARE_BIN=${ELF_DIR}/sensorweb-firmware.bin
+
+set -x
+cc3200tool -p /dev/ttyUSB1 --sop2 ~dtr --reset prompt write_file ${FIRMWARE_BIN} /sys/mcuimg.bin
---
+++ src/main.rs
@@ -0,0 +1,111 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// We won't use the usual `main` function. We are going to use a different "entry point".
+#![no_main]
+
+// We won't use the standard library because it requires OS abstractions like threads and files and
+// those are not available in this platform.
+#![no_std]
+#![feature(alloc)]
+
+extern crate cc3200;
+extern crate alloc;
+extern crate freertos_rs;
+extern crate freertos_alloc;
+
+use cc3200::cc3200::{Board, Utils, LedEnum, LedName};
+
+use alloc::arc::Arc;
+use freertos_rs::{CurrentTask, Duration, Task, Queue};
+
+// Conceptually, this is our program "entry point". It's the first thing the microcontroller will
+// execute when it (re)boots. (As far as the linker is concerned the entry point must be named
+// `start` (by default; it can have a different name). That's why this function is `pub`lic, named
+// `start` and is marked as `#[no_mangle]`.)
+//
+// Returning from this function is undefined because there is nothing to return to! To statically
+// forbid returning from this function, we mark it as divergent, hence the `fn() -> !` signature.
+#[no_mangle]
+pub fn start() -> ! {
+
+    Board::init();
+
+    Board::init_term();
+    Board::clear_term();
+    Board::message("CC3200 Sample code\n");
+
+    let queue = Arc::new(Queue::new(10).unwrap());
+    let _producer = {
+        let queue = queue.clone();
+        Task::new()
+            .name("producer")
+            .start(move || {
+                let msgs = ["Welcome ", "to ", "CC32xx ", "development !\n"];
+                loop {
+                    for msg in msgs.iter() {
+                        queue.send(msg, Duration::ms(15)).unwrap();
+                        CurrentTask::delay(Duration::ms(15))
+                    }
+                    CurrentTask::delay(Duration::ms(1000))
+                }
+            })
+            .unwrap()
+    };
+
+    let _consumer = {
+        let queue = queue.clone();
+        Task::new()
+            .name("consumer")
+            .start(move || {
+                loop {
+                    let msg = queue.receive(Duration::ms(2000)).unwrap();
+                    Board::message("Received: ");
+                    Board::message(msg);
+                    Board::message("\n");
+                }
+            })
+            .unwrap()
+    };
+
+    let _blinky = {
+        Task::new()
+            .name("blinky")
+            .start(|| {
+                Board::led_configure(&[LedEnum::LED1, LedEnum::LED2, LedEnum::LED3]);
+                Board::led_off(LedName::MCU_ALL_LED_IND);
+                let mut counter = 0;
+                loop {
+                    Board::led_on(LedName::MCU_RED_LED_GPIO);
+                    if counter & 1 != 0 {
+                        Board::led_on(LedName::MCU_ORANGE_LED_GPIO);
+                    } else {
+                        Board::led_off(LedName::MCU_ORANGE_LED_GPIO);
+                    }
+                    if counter & 2 != 0 {
+                        Board::led_on(LedName::MCU_GREEN_LED_GPIO);
+                    } else {
+                        Board::led_off(LedName::MCU_GREEN_LED_GPIO);
+                    }
+                    Utils::delay(1333333);
+                    Board::led_off(LedName::MCU_RED_LED_GPIO);
+                    Utils::delay(1333333);
+                    Board::led_on(LedName::MCU_RED_LED_GPIO);
+                    Utils::delay(1333333);
+                    Board::led_off(LedName::MCU_RED_LED_GPIO);
+                    Utils::delay(1333333 * 6);
+
+                    counter += 1;
+                }
+            })
+            .unwrap()
+    };
+
+    Board::start_scheduler();
+
+    // The only reason start_scheduler should fail is if there wasn't enough
+    // heap to initialize the IDLE and timer tasks
+
+    loop {}
+}
