07ed7f52a61bb7305d4c4008e25f81ff2cfade61
Fabrice Desré <fabrice@desre.org>
2016-12-01 11:41:01 -0800

    Fix for clippy warnings in microjson (#44)

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,7 +4,7 @@ curl https://sh.rustup.rs -sSf -o rustup.sh
 chmod +x ./rustup.sh
 ./rustup.sh -y
 export PATH=/home/travis/.cargo/bin:$PATH
-rustup override set nightly-2016-11-06
+rustup override set nightly-2016-11-25
 rustup component add rust-src
 cargo install xargo
 cargo --version
