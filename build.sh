a245ad26205df59ffa96bcaf2944061c07b14dc7
Fabrice Desré <fabrice@desre.org>
2017-01-20 17:46:13 -0800

    Addressing comments

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 OPT=debug
 for opt in $@; do
     if [ x"$opt" == x"--release" ]; then
@@ -15,6 +17,6 @@ FIRMWARE_BIN=${ELF_DIR}/sensorweb-firmware.bin
 rm -f ${FIRMWARE_ELF}
 rm -f ${FIRMWARE_BIN}
 
-xargo build --target=${TARGET} "$@" && \
-arm-none-eabi-size ${FIRMWARE_ELF} && \
+xargo build --target=${TARGET} "$@"
+arm-none-eabi-size ${FIRMWARE_ELF}
 arm-none-eabi-objcopy -O binary ${FIRMWARE_ELF} ${FIRMWARE_BIN}
