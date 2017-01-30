a2a9898c1768748be9c9ffbd304e8c4902096eaf
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-01-30 18:10:13 +0100

    Add port argument to helper scripts
    The command-line option --port allows for setting the serial port
    at which the cc3200 device is connected to the computer.

--- flash.sh
+++ flash.sh
@@ -3,4 +3,4 @@
 . ./parse-args.sh
 
 set -x
-cc3200tool -p /dev/ttyUSB1 --sop2 ~dtr --reset prompt write_file ${FIRMWARE_BIN} /sys/mcuimg.bin
+cc3200tool -p ${PORT} --sop2 ~dtr --reset prompt write_file ${FIRMWARE_BIN} /sys/mcuimg.bin
